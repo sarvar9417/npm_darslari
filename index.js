792afae8507672d77c08933a5ee209d134d96af9
sarvar9417 <69727326+sarvar9417@users.noreply.github.com>
2021-07-28 07:26:24 +0500

    Initsial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+let _ = require('underscore')
+
+// require parametrining ko'rinishlari
+// 1. Core module
+// 2. fayl yoki papka
+// 3. node_modules
+
+let result = _.contains([3, 5, 9], 5)
+
+console.log(result)
